374b23ec9ede23016bdd36a39a0492dba5cc9a75
Yassir-BenBOUBKER <97686091+Yassir-BenBOUBKER@users.noreply.github.com>
2022-06-09 16:38:41 +0100

    enh(drawio): Display real element size by adding border in Editor #40 (#41)
    * Display real element size in Editor
    
    * fix
    
    * fixes
    
    * fixes 2
    
    * fixes 3

--- src/main/webapp/js/grapheditor/Graph.js
+++ src/main/webapp/js/grapheditor/Graph.js
@@ -1237,6 +1237,17 @@ Graph = function(container, model, renderHint, stylesheet, themes, standalone)
 			}
 			
 			ch.destroyFocusHighlight();
+
+			var cells = this.getSelectionCells();
+			if (cells !== null && cells.length > 0)
+			{
+				cells.forEach((cell) => {
+					if (!this.model.isEdge(cell)) {
+						this.setCellStyles(mxConstants.STYLE_IMAGE_BORDER, '#000000', cells);
+					}
+				});
+			}
+
 		}));
 		
 		// Initializes touch interface
@@ -10636,7 +10647,7 @@ if (typeof mxVertexHandler !== 'undefined')
 		{
 			var select = null;
 
-			if (cells != null && cells.length > 0)
+			if (cells !== null && cells.length > 0)
 			{
 				this.model.beginUpdate();
 				try
